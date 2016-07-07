a3214d6a539419083463545954c2a067cee2a083
sspenst <spencerspenst@gmail.com>
2016-07-07 14:40:46 -0700

    779: Indices are now stored corresponding to values. SST now returns the number of elements instead of the device memory address

--- sst_simple.cu
+++ sst_simple.cu
@@ -5,7 +5,7 @@
 
 #define SIZE(A) A*sizeof(int)
 #define FSIZE(A) A*sizeof(float)
-#define LENGTH 8
+#define LENGTH 8 
 
 // Test the new SST instruction's functionality
 __global__ void SSTTest(float* V, float* R, int* addr, int N) {
@@ -31,35 +31,36 @@ __global__ void SSTTest(float* V, float* R, int* addr, int N) {
 }
 
 int main(int argc, char** argv) {
-	float *h_vector = (float*)calloc(LENGTH, FSIZE(1));
+	float *h_vector = (float*)calloc(2*LENGTH, FSIZE(1)); // twice as long to store values as well as indices
 	float *h_result = (float*)malloc(FSIZE(LENGTH));
 	int *h_addr = (int*)malloc(SIZE(1));
 
 	for (int i = 0; i < LENGTH; i++) {
 		if (i % 2 == 1) h_vector[i] = 0.0;
 		else h_vector[i] = i + 1.0;
+		h_vector[i+LENGTH] = 0.0;
 	}
 
 	float *d_vector, *d_result;
-	cudaMalloc(&d_vector, FSIZE(LENGTH));
+	cudaMalloc(&d_vector, FSIZE(2*LENGTH));
 	cudaMalloc(&d_result, FSIZE(LENGTH));
 	int *d_addr;
 	cudaMalloc(&d_addr, SIZE(1));
 
-	cudaMemcpy(d_vector, h_vector, FSIZE(LENGTH), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_vector, h_vector, FSIZE(2*LENGTH), cudaMemcpyHostToDevice);
 
 	SSTTest<<<1, LENGTH>>>(d_vector, d_result, d_addr, LENGTH);
 	
-	cudaMemcpy(h_vector, d_vector, FSIZE(LENGTH), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_vector, d_vector, FSIZE(2*LENGTH), cudaMemcpyDeviceToHost);
 	cudaMemcpy(h_result, d_result, FSIZE(LENGTH), cudaMemcpyDeviceToHost);
 	cudaMemcpy(h_addr, d_addr, SIZE(1), cudaMemcpyDeviceToHost);
 
+	printf("\nValues: \tIndices:\n");
 	for (int i = 0; i < LENGTH; i++) {
-		printf("%f\t", h_vector[i]);
-		printf("%f\n", h_result[i]);
+		printf("%f\t%d\n", h_vector[i], (int)h_vector[i+LENGTH]);
 	}
-	
-	printf("\nEnd address: 0x%d\n", h_addr[0]);
+	printf("\nSST return value: %d\n", h_addr[0]);
+	printf("Array start: %p\nArray end: %p\n", &h_vector[0], &h_vector[h_addr[0]]);
 
 	cudaFree(d_vector);
 	cudaFree(d_result);
